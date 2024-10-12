5903d5471924cacb390e15d3717a7a67826a7ccc
Ekaterina Tyazhova <ekaterina171102@gmail.com>
2024-10-12 22:29:30 +0500

    текст комментария

--- the_snake.py
+++ the_snake.py
@@ -252,9 +252,9 @@ if __name__ == '__main__':
 #                 game_object.next_direction = UP
 #             elif event.key == pygame.K_DOWN and game_object.direction != UP:
 #                 game_object.next_direction = DOWN
-#             elif event.key == pygame.K_LEFT and game_object.direction != RIGHT:
+#         elif event.key == pygame.K_LEFT and game_object.direction != RIGHT:
 #                 game_object.next_direction = LEFT
-#             elif event.key == pygame.K_RIGHT and game_object.direction != LEFT:
+#          elif event.key == pygame.K_RIGHT and game_object.direction != LEFT:
 #                 game_object.next_direction = RIGHT
 
 # Метод обновления направления после нажатия на кнопку
